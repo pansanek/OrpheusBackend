dc0ae82390cc9894dc6ce0412133d8a337e43ba2
SashaPotemkin <alex.potjomkin@gmail.com>
2024-05-21 14:20:24 +0300

    db connection user

--- app_user/app/main.py
+++ app_user/app/main.py
@@ -2,10 +2,11 @@ import asyncio
 
 from fastapi import FastAPI
 
+from app_user.app.schemas.base_schema import engine
 from app_user.app.endpoints.user_router import user_router
-
+from app_user.app.schemas import user_schema
 app = FastAPI(title='App')
-
+user_schema.Base.metadata.create_all(bind=engine)
 
 @app.on_event('startup')
 def startup():
